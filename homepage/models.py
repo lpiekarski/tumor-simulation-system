2ed3eb679a31e554b1127ca5dfca784d2ddf5f55
Łukasz Piekarski <lp394685@students.mimuw.edu.pl>
2019-12-23 15:16:33 +0100

    #27 Grupy dla elementów karuzeli

--- homepage/models.py
+++ homepage/models.py
@@ -1,12 +1,28 @@
 from django.db import models
-from django.urls import reverse
+from django.contrib.auth.models import Group
 
 from core.models import BaseModel
 from core.utils import media_file_path
 
 
 class Carousel(BaseModel):
-    image = models.ImageField(upload_to=media_file_path, blank=True, null=True)
-    content = models.TextField()
-    is_public = models.BooleanField()
+    image = models.ImageField(
+        upload_to=media_file_path,
+        blank=True,
+        null=True
+    )
 
+    content = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    is_public = models.BooleanField(
+        blank=True,
+        null=True
+    )
+
+    groups = models.ManyToManyField(
+        to=Group,
+        blank=True
+    )
