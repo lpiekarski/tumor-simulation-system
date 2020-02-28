68bf71d01b1da81f4fe9b3e841b8667750a28b83
Piotr Kowalkowski <piotrpkowalkowski@gmail.com>
2020-02-28 13:02:07 +0100

    Add Protocol and ProtocolDose models

--- tumor/models.py
+++ tumor/models.py
@@ -10,3 +10,22 @@ class InitialTumor(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Protocol(models.Model):
+    name = models.CharField(max_length=100, blank=False)  # TODO: add validation for correct protocol
+
+    def __str__(self):
+        return self.name
+
+    def serialize(self):
+        return self.name  # TODO: serialize self.dose_set
+
+
+class ProtocolDose(models.Model):
+    protocol = models.ForeignKey(to='Protocol', on_delete=models.CASCADE, related_name='dose', null=False)
+    time = models.IntegerField(verbose_name='Time of dose', null=False)
+    dose = models.DecimalField(verbose_name='Dose', null=False, decimal_places=2, max_digits=4)
+
+    class Meta:
+        verbose_name = 'Protocol Dose'
