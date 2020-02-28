15d693ad9ad9df53c09a3e0a5a7d2a6336e9d5d3
Piotr Kowalkowski <piotrpkowalkowski@gmail.com>
2020-02-28 12:42:19 +0100

    Add verbose name and convenient name display in django admin

--- tumor/models.py
+++ tumor/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class InitialTumor(models.Model):
     name = models.CharField(max_length=100, blank=False)
     file = models.FileField(upload_to='tumors/')  # TODO: add validation check for correct tumor file
+
+    class Meta:
+        verbose_name = 'Initial Tumor'
+
+    def __str__(self):
+        return self.name
