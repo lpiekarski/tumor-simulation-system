61a20ebe4829937d33519077302a50d2511136a1
Łukasz Piekarski <lp394685@students.mimuw.edu.pl>
2020-03-04 20:02:28 +0100

    Fixed static files serving from last commit

--- settings/base.py
+++ settings/base.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django.contrib.humanize',
     'core',
     'homepage',
