3806123959d9d8e0cf8ec7801c63cccd55a816ed
lpiekarski <lp394685@students.mimuw.edu.pl>
2020-03-04 02:59:07 +0000

    Configured settings for deploy

--- settings/base.py
+++ settings/base.py
@@ -126,8 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATICFILES_DIRS = []
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
