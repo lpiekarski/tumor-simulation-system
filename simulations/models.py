1c65c58cf0ab6db6202850ced88cc4c619c642ca
lpiekarski <wookashp98@gmail.com>
2020-04-20 08:22:16 -0400

    Added simulation models and basic dashboard view

--- simulations/models.py
+++ simulations/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from protocols.models import Protocol
 
 
 class InitialTumor(models.Model):
@@ -11,3 +12,40 @@ class InitialTumor(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Simulation(models.Model):
+    name = models.CharField(max_length=100, blank=False)  # TODO: add validation for correct protocol
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='simulations', null=False)
+    protocol = models.ForeignKey(Protocol, on_delete=models.CASCADE, related_name='simulations', null=False)
+    description = models.TextField(null=False)
+    time_duration = models.FloatField(null=False)
+
+    class Meta:
+        verbose_name = 'Tumor Simulation'
+
+    def __str__(self):
+        return self.name
+
+
+class SimulationState(models.Model):
+    simulation = models.ForeignKey(to='Simulation', on_delete=models.CASCADE, related_name='state', null=False)
+    time = models.IntegerField(verbose_name='Time', null=False)
+    _W = models.TextField(null=False)
+    _CHO = models.TextField(null=False)
+    _OX = models.TextField(null=False)
+    _GI = models.TextField(null=False)
+    _timeInRepair = models.TextField(null=False)
+    _irradiation = models.TextField(null=False)
+    _cellState = models.TextField(null=False)
+    _cellCycle = models.TextField(null=False)
+    _proliferationTime = models.TextField(null=False)
+    _cycleChanged = models.TextField(null=False)
+    _G1time = models.TextField(null=False)
+    _Stime = models.TextField(null=False)
+    _G2time = models.TextField(null=False)
+    _Mtime = models.TextField(null=False)
+    _Dtime = models.TextField(null=False)
+
+    class Meta:
+        verbose_name = 'Simulation State'
--- simulations/views.py
+++ simulations/views.py
@@ -1,10 +1,14 @@
 from guardian.shortcuts import get_objects_for_user
-
+from simulations.models import Simulation, SimulationState
 from core.utils import render_with_context
 
 
 def dashboard(request, template_name="simulations/dashboard.html"):
-    return render_with_context(request, template_name, {})
+    simulations = get_objects_for_user(
+        request.user,
+        "simulations.view_simulation"
+    ).union(Simulation.objects.filter(author=request.user))
+    return render_with_context(request, template_name, {"simulations": simulations})
 
 
 def simulation_view(request, simulation, template_name="simulations/simulation.html"):
